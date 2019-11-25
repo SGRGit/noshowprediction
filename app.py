efba37647bada59a0f9607d69b1f303750648951
SGRGit <50324151+SGRGit@users.noreply.github.com>
2019-11-25 15:53:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,16 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    '''
-    For rendering results on PHP GUI
-    '''
-    int_features =[int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = round(model.predict_proba(final_features)[0][0] *100, 2)
-    
-    output = prediction 
-    
-    return render_template('index.html', prediction_text = 'Appointment Chance  {} %'.format(output))
+if request.method == 'POST':
+    def predict():
+        ''' 
+        For rendering results on PHP GUI
+        '''
+        int_features =[int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = round(model.predict_proba(final_features)[0][0] *100, 2)    
+        output = prediction 
+        return render_template('index.html', prediction_text = 'Appointment Chance  {} %'.format(output))
 
 
 if __name__ == "__main__":
